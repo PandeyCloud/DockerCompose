148fc9e5badd923ee4e423cf32c0f9ec7ef6187e
Ganesh Pandey <gpandey@markem-imaje.com>
2022-01-28 10:38:55 +0530

    Base code for docker compose [Dot net core]Web front end -> Web API

---
+++ WebFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Hello from webfrontend";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                //request.RequestUri = new Uri("http://localhost:32316/weatherforecast");
+                request.RequestUri = new Uri("http://mywebapi:32316/weatherforecast");
+                var response = await client.GetAsync(request.RequestUri);
+                ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
